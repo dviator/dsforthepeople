db84c545a6243986ba52e478e03892987330f59e
Alex Stein <astein.ny@gmail.com>
2016-05-30 14:32:44 -0500

    locationcounter outputting proper CSV format into data/etl_output

--- etl/locationcounter.py
+++ etl/locationcounter.py
@@ -6,7 +6,7 @@ import os
 # for efficient searching
 import re
 
-# for importing csv files
+# for importing/writing csv files
 import csv
 
 # for multiprocessing
@@ -15,6 +15,12 @@ from multiprocessing import Pool
 import sys
 import time
 
+# Global Variables
+# global article_info
+global COUNTRIES
+global OUTFILE
+
+
 #setup
 logging.basicConfig(filename='LocationCounter.log',level=logging.INFO,format='%(asctime)s %(levelname)s: %(message)s')
 
@@ -22,8 +28,13 @@ config = configparser.ConfigParser()
 config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)),"../","crawlers/","newscrawler.conf"))
 
 data_root_dir = config.get('parsearticle','data_root_dir')
+OUTFILE = data_root_dir + "/etl_output/article_counts.csv"
 
-#Fetch list of locations.
+# Delete the output file if it currently exists
+if os.path.isfile(OUTFILE):
+	os.remove(OUTFILE)
+
+#Fetch list of locations with corrresponding ISO country code
 def getCountries(): 
 	countries = {}
 	locations_fn = data_root_dir + "/locations/country_codes.csv"
@@ -33,67 +44,80 @@ def getCountries():
 			countries[row[0]] = row[1]
 	return countries
 
-#Container (Dictionary of Dictionaries) to store information
-def initialize_container():
-	container = {}
-	countries = getCountries()
-	for place in countries:
-		container[place] = {
-			"code": countries[place],
-			"count": {}
-		}
-	return container
+COUNTRIES = getCountries()
 
-# Function to process each file
-# def process_file(container, articleid):
-def process_file(articleid):
+#Functions for output file formatting
+def writeMetadataHeader():
+	with open(OUTFILE, 'a') as csvfile:
+		articleWriter = csv.writer(csvfile, delimiter='~',quoting=csv.QUOTE_ALL)
+		articleWriter.writerow(["Title","Date","URL","Authors","Source","fullTextID","Country","Count"])
+	
+	return
+
+def writeMetadataRow(articleid, country, count):
+	#Logic for writing a metadata row, needs to be moved to a single threaded function
+	csv_start = time.time()
+	logging.debug("Metadata path is {}".format(OUTFILE))
+
+	#FIXME: temporarily fix these input variables to zero since no metadata is yet available
+	title = ""
+	date = ""
+	url = ""
+	authors = ""
+	newsSource = ""
+
+	#Write metadata and reference to full text file name to csv separated by ~ character
+	with open(OUTFILE, 'a') as csvfile:
+		articleWriter = csv.writer(csvfile, delimiter='~',quoting=csv.QUOTE_ALL)
+		articleWriter.writerow([title,date,url,authors,newsSource,articleid,country,count])
+	logging.debug("CSV write completed to {} in {} seconds".format(OUTFILE,time.time()- csv_start))
+	
+	return
+
+def process_file(article_path):
 	''' Process one file: count occurrences of country names in this file '''
-	container = initialize_container() #FIXME: need NOT to initialize container every time I process a file!!!
-	with open(articleid, 'r') as inp:
-		for line in inp:
-			for place in container:
-				if re.search(str(place), line, re.IGNORECASE):
-					if articleid in container[place]["count"]:
-						container[place]["count"][articleid] += 1
-					else:
-						container[place]["count"][articleid] = 0
-
-# partial_process_file = partial(process_file, container=initialize_container())
+	articleid = os.path.basename(os.path.normpath(article_path))
+	with open(article_path) as inp:
+		lines = inp.readlines()
+	# For each country, search every line of the input file for it's occurrence
+	for place in COUNTRIES:
+		flag = False
+		count = 0
+		for line in lines:
+			if re.search(place, line, re.IGNORECASE):
+				flag = True
+				count += 1
+		# If this country's name was found at all, print it's count to the output file
+		if flag:
+			writeMetadataRow(articleid, COUNTRIES[place], str(count))
+			# outp.write("\n" + COUNTRIES[place] + "," + str(count))
 
 # Function to process all files in parallel
-# def process_files_parallel(arg, dirname, names):
 def process_files_parallel(paths):
 	''' process each file in via map() '''
 	print ("inside process_files_parallel()")
 	pool=Pool()
 	results=pool.map(process_file, paths)
-	# results=pool.map(partial_process_file, paths)
-	# results=pool.map(process_file_partial, [os.path.join(dirname, name) for name in names])
 
 #Convert to lowercase
 
 #Determine which dataset to work with.
 
-#Open a file.
-# filename = data_root_dir + "/nytimes/fullText/" + "4978001668131110631"
-# contents = []
-# with open(filename) as article:
-#     for line in article:
-#         contents.append(line)
-
 # Main
-# if __name__ == '__main__':
-data_dir = data_root_dir + "/nytimes/fullText/"
-start=time.time()
-paths=[]
-for root, dirs, files in os.walk(data_dir, topdown=False):
-    for name in files:
-        paths.append(os.path.join(root, name))
-    for name in dirs:
-        paths.append(os.path.join(root, name))
-process_files_parallel(paths)
-# os.walk(data_dir, process_files_parallel, None)
-print ("process_files_parallel() ",time.time()-start,sep=" ")
+if __name__ == '__main__':
+	data_dir = data_root_dir + "/nytimes/fullText/"
+	start=time.time()
+	paths=[]
+	writeMetadataHeader()
+	for root, dirs, files in os.walk(data_dir, topdown=False):
+	    for name in files:
+	        paths.append(os.path.join(root, name))
+	    for name in dirs:
+	        paths.append(os.path.join(root, name))
+	process_files_parallel(paths)
+	# os.walk(data_dir, process_files_parallel, None)
+	# print (article_info)
+	print ("process_files_parallel() ",time.time()-start,sep=" ")
 
 # print ("counts are: ", location_counts)
 #join filename to entry in metadata table/database row

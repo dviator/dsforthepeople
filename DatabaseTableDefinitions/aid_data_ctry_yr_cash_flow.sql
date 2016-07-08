64338f5b4c77a362cf45227089a655e1950b9118
michaelgreis <michaelgreis@gmail.com>
2016-07-07 21:46:44 -0500

    Update aid_data_ctry_yr_cash_flow.sql
    Added case statements to clean up countries

--- DatabaseTableDefinitions/aid_data_ctry_yr_cash_flow.sql
+++ DatabaseTableDefinitions/aid_data_ctry_yr_cash_flow.sql
@@ -32,10 +32,25 @@ FROM
 	FROM (
 		--grabs the donations for each country, filters bad data.
 		SELECT year,
-		 TRIM(donor_iso3) AS CTRY_ISO3,
+		--Case statement to handle exceptions in the 2007 data
+		 CASE WHEN donor_iso3 = 'TW ' THEN 'TWN'
+			WHEN donor_iso3 = 'LD' THEN 'NLD'
+			WHEN donor_iso3 = ' OR' THEN 'NOR'
+			WHEN donor_iso3 = ' ZL' THEN 'NZL'
+			WHEN donor_iso3 = 'CA ' THEN 'CAN'
+			WHEN donor_iso3 = 'D K' THEN 'DNK'
+			WHEN donor_iso3 = 'FI ' THEN 'FIN'
+			WHEN donor_iso3 = 'HU ' THEN 'HUN'
+			WHEN donor_iso3 = 'I D' THEN 'IND'
+			WHEN donor_iso3 = 'JP ' THEN 'JPN'
+			WHEN donor_iso3 = 'SV ' THEN 'SVN'
+			WHEN donor_iso3 = 'TW ' THEN 'TWN'
+			ELSE donor_iso3
+			END AS CTRY_ISO3,
 		 SUM(commitment_amount_usd_constant) AS CASH_FLOW,
 		 'D' AS DONOR_RECEIVER_FLG
 		FROM launch_pad.aid_data
+		--Commented out the two lines below in order to allow exception records to flow through.
 		WHERE donor_iso3 IS NOT NULL
 			AND donor_iso3 NOT IN ('  ','')
 			AND commitment_amount_usd_constant IS NOT NULL
@@ -43,7 +58,44 @@ FROM
 		UNION ALL
 		--grabs the receiveing for each country, filters out bad data.
 		SELECT year,
-		 TRIM(recipient_iso3) AS CTRY_ISO3,
+			CASE WHEN recipient_iso3 = ' AM' THEN 'NAM'
+				WHEN recipient_iso3 = ' ER' THEN 'NER'
+				WHEN recipient_iso3 = ' GA' THEN 'NGA'
+				WHEN recipient_iso3 = ' IC' THEN 'NIC'
+				WHEN recipient_iso3 = ' IU' THEN 'NIU'
+				WHEN recipient_iso3 = ' PL' THEN 'NPL'
+				WHEN recipient_iso3 = ' RU' THEN 'NRU'
+				WHEN recipient_iso3 = 'BE ' THEN 'BEN'
+				WHEN recipient_iso3 = 'BT ' THEN 'BTN'
+				WHEN recipient_iso3 = 'CA ' THEN 'CAN'
+				WHEN recipient_iso3 = 'CH ' THEN 'CHN'
+				WHEN recipient_iso3 = 'G B' THEN 'GNB'
+				WHEN recipient_iso3 = 'G Q' THEN 'GNQ'
+				WHEN recipient_iso3 = 'GI ' THEN 'GIN'
+				WHEN recipient_iso3 = 'H D' THEN 'HND'
+				WHEN recipient_iso3 = 'HU ' THEN 'HUN'
+				WHEN recipient_iso3 = 'I D' THEN 'IND'
+				WHEN recipient_iso3 = 'ID ' THEN 'IDN'
+				WHEN recipient_iso3 = 'IR ' THEN 'IRN'
+				WHEN recipient_iso3 = 'JP ' THEN 'JPN'
+				WHEN recipient_iso3 = 'K A' THEN 'KNA'
+				WHEN recipient_iso3 = 'KE ' THEN 'KEN'
+				WHEN recipient_iso3 = 'LB ' THEN 'LBN'
+				WHEN recipient_iso3 = 'M E' THEN 'MNE'
+				WHEN recipient_iso3 = 'M G' THEN 'MNG'
+				WHEN recipient_iso3 = 'OM ' THEN 'OMN'
+				WHEN recipient_iso3 = 'P G' THEN 'PNG'
+				WHEN recipient_iso3 = 'PA ' THEN 'PAN'
+				WHEN recipient_iso3 = 'SD ' THEN 'SDN'
+				WHEN recipient_iso3 = 'SE ' THEN 'SEN'
+				WHEN recipient_iso3 = 'SH ' THEN 'SHN'
+				WHEN recipient_iso3 = 'SV ' THEN 'SVN'
+				WHEN recipient_iso3 = 'TO ' THEN 'TON'
+				WHEN recipient_iso3 = 'TU ' THEN 'TUN'
+				WHEN recipient_iso3 = 'V M' THEN 'VNM'
+				WHEN recipient_iso3 = 'VE ' THEN 'VEN'
+				ELSE recipient_iso3
+			END AS CTRY_ISO3,
 		 SUM(commitment_amount_usd_constant * -1) AS CASH_FLOW,
 		 'R' AS DONOR_RECEIVER_FLG
 		FROM launch_pad.aid_data

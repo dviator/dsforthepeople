73409fc4083d7cce923733d3f8701222ccb7f307
Alex Stein <astein.ny@gmail.com>
2016-07-23 15:32:01 -0500

    fixed test for updated locationcounter.py

--- tests/testcounter.py
+++ tests/testcounter.py
@@ -5,6 +5,10 @@ import logging
 from etl import locationcounter
 import time
 
+## Usage ##
+# cd dsforthepeople
+# python3 -m unittest -v tests.testcounter
+
 class TestCounter(unittest.TestCase):
 
 		def setUp(self):
@@ -12,10 +16,11 @@ class TestCounter(unittest.TestCase):
 
 		def test_counting_proper_number_of_mentions_in_files(self):
 
-			outfile = "/test/article_counts_test.csv"
+			outdir = "/test/"
+			outfile = "article_counts_test.csv"
 			metadatafile = "/test/metadata_test.csv"
 			data_dir = "/test/testdata"
 
-			locationcounter.main(outfile, metadatafile, data_dir)
+			locationcounter.main(outdir, outfile, metadatafile, data_dir)
 			
 			assert True

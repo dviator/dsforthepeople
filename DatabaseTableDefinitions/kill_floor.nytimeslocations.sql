41831d0ac5f984e0a9d1a42c00090c54f75f8104
Dan Veenstra <danielveenstra11@gmail.com>
2016-07-31 18:53:53 -0500

    Updated nytimeslocations.sql with /copy command

--- DatabaseTableDefinitions/kill_floor.nytimeslocations.sql
+++ DatabaseTableDefinitions/kill_floor.nytimeslocations.sql
@@ -6,3 +6,8 @@ Source varchar(10),
 fullTextId varchar(32),
 Country varchar(3),
 Count smallint)
+
+#Command line to load table from newscrawler instance
+#I omitted Authors column because Arrays were tricky and we didn't need that data
+#Ran in <30 seconds on newscrawler instance
+\copy kill_floor.nytimeslocations from '/home/danielveenstra/articlecountsnoauthor.csv' with csv delimiter '~' quote '"' header

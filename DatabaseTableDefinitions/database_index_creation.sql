1ecdacf61d4e6484705c60e0cb40d4f1bf1050a8
Michael Greis <michaelgreis@gmail.com>
2016-10-20 20:47:30 -0500

    Update database_index_creation.sql

--- DatabaseTableDefinitions/database_index_creation.sql
+++ DatabaseTableDefinitions/database_index_creation.sql
@@ -3,3 +3,5 @@
 CREATE INDEX country_date_index ON launch_pad.aid_data_3 (recipient_iso2, start_date);
 
 CREATE INDEX country_date_location_index ON kill_floor.nytimeslocations (country, date);
+
+CREATE INDEX country_date_location_index ON launch_pad.country_location_count (year, country_iso2);
